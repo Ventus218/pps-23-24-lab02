923e46bd7c37ccadc1e50ad0df01a203e8480213
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-14 21:35:20 +0100

    fixed style of ex 5

--- src/main/scala/task2/Es5.scala
+++ src/main/scala/task2/Es5.scala
@@ -2,13 +2,13 @@ package task2;
 
 object Es5 extends App:
 
-    def compose(f: Int => Int, g: Int => Int): Int => Int = x =>
-        f(g(x))
+    def compose(f: Int => Int, g: Int => Int): Int => Int =
+        x => f(g(x))
 
     println(compose(_ - 1, _ * 2)(5))
 
-    def genericCompose[FOut, GIn, GOut](f: GOut => FOut, g: GIn => GOut): GIn => FOut = x =>
-        f(g(x))
+    def genericCompose[FOut, GIn, GOut](f: GOut => FOut, g: GIn => GOut): GIn => FOut =
+        x => f(g(x))
 
     println(genericCompose((fIn: Int) => fIn - 1, (gIn: Int) => gIn * 2)(5))
     
